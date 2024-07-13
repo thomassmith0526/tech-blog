ed6dc86af1daa20e57f62d412d891d77d5bac69d
thomassmith0526 <thomassmith0526@gmail.com>
2024-07-13 10:11:33 -0500

    main-handlebar

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const exphbs =  require('express-handlebars');
+const sequelize = require('./config/connection')
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.json(_dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+});
